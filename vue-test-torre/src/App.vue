ed878e3f5d7257865438b7f70aa1272982907de2
JonathanAleksei <j.suarez.a.garcia@gmail.com>
2023-04-10 08:37:41 -0500

    button test

--- vue-test-torre/src/App.vue
+++ vue-test-torre/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <h1>Torre - User Skills </h1><br>
-
+  <button @click="fetchUser">Show Skills</button><br>
   <ul>
     <li>Skills</li>
   </ul>
 </template>
 
 <script>
-
+export default {
+  methods: {
+    fetchUser () {
+      console.log("Skills")
+    }
+  }
+}
 </script>
 
 <style scoped>
